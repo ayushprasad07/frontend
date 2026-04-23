3de75922b87ab5c7146e5d476d8393365b4277ea
ayushprasad07 <ayushprasad2110@gmail.com>
2026-04-23 12:05:08 +0530

    Some changes

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -125,7 +125,11 @@ router.post(
 });
 
 router.get("/logout", (req, res) => {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    });
     res.status(200).json({success: true, message: "User logged out" });
 })
 
